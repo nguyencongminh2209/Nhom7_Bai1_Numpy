1c95feb4165e3669fc3ec4824da0d407f0596dc3
Minh <145255015+nguyencongminh2209@users.noreply.github.com>
2023-09-19 09:17:13 +0700

    MinhUpdate.py
    okok

--- Source.py
+++ Source.py
@@ -2,7 +2,8 @@ import numpy as np
 
 # Nhập số phương trình và số ẩn (n) từ người dùng
 n = int(input("Nhập số phương trình và số ẩn (n): "))
-while (n <= 1) :
+while (n < 2) :
+    #NẾU ĐỂ <= 1 CÓ THỂ LẤY SỐ KHÔNG PHẢI SỐ TỰ NHIÊN
     n = int(input("Đây không phải hệ phương trình, vui lòng nhập lại: "))
 # Khởi tạo ma trận hệ số A và vector kết quả B
 A = np.zeros((n, n))
