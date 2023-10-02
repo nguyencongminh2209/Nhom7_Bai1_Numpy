c0f2350afcabf45d526747644ce08c430ea153ee
Quân <116066343+quankenz@users.noreply.github.com>
2023-10-02 22:48:31 +0700

    Quan update EquationTab.py

--- nhom5_vedothihamso/nhom5_vedothihamso/VD/EquationTab.py
+++ nhom5_vedothihamso/nhom5_vedothihamso/VD/EquationTab.py
@@ -1,11 +1,8 @@
 import os
-
 import PyQt6.QtWidgets
 import matplotlib.colors as mcolors
 import sympy
-
 from .Equation import Equation
-
 
 class EquationTab(PyQt6.QtWidgets.QWidget):
     colors = tuple(mcolors.TABLEAU_COLORS.keys())
@@ -13,48 +10,44 @@ class EquationTab(PyQt6.QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        # create layout
+        # Create layout
         self.plot = None
         self.layout = PyQt6.QtWidgets.QVBoxLayout()
 
-        # add label
+        # Add label
         self.label = PyQt6.QtWidgets.QLabel("Equations")
 
-        # add label to layout
+        # Add label to layout
         self.layout.addWidget(self.label)
 
-        # add button
+        # Add button
         self.button = PyQt6.QtWidgets.QPushButton("Add Equation")
 
-        # add equation when button is clicked
+        # Add equation when button is clicked
         self.button.clicked.connect(self.add_equation)
 
-        self.refresh_button = PyQt6.QtWidgets.QPushButton("Refesh")
+        self.refresh_button = PyQt6.QtWidgets.QPushButton("Refresh")
         self.refresh_button.clicked.connect(self.refresh)
 
-        # add button to layout
+        # Add buttons to layout
         self.layout.addWidget(self.button)
         self.layout.addWidget(self.refresh_button)
 
-        # set layout
+        # Set layout
         self.setLayout(self.layout)
 
     def add_equation(self):
-        # create equation
+        # Create equation
         equation = Equation()
 
-        # add equation to the second to last position of the layout
+        # Add equation to the second to last position of the layout
         self.layout.insertWidget(self.layout.count() - 2, equation)
 
     def refresh(self):
-
-        # loop over all Equation
+        # Loop over all Equation widgets
         plot = None
-        for color, equation in zip(self.colors, self.children()):
-
-            if type(equation) != Equation:
-                continue
-            equation: Equation = equation
+        for widget in self.findChildren(Equation):
+            equation = widget
             if equation.equation_right is None:
                 continue
             lhs, rhs = equation.equation_left, equation.equation_right
@@ -63,13 +56,14 @@ class EquationTab(PyQt6.QtWidgets.QWidget):
             for f in sympy.solve(lhs - rhs, sympy.var("y")):
                 label = f'y = {f}'
                 if plot is None:
-                    plot = sympy.plotting.plot(f, show=False, line_color=color, ylabel="y", legend=True, label=label)
+                    plot = sympy.plotting.plot(f, show=False, line_color=self.colors[len(plot.series)], ylabel="y", legend=True, label=label)
                 else:
-                    plot.extend(
-                        sympy.plotting.plot(f, show=False, line_color=color, ylabel="y", legend=True, label=label))
+                    plot.extend(sympy.plotting.plot(f, show=False, line_color=self.colors[len(plot.series)], ylabel="y", legend=True, label=label))
+        
         if plot is not None:
-            if not os.path.exists("cache"):
-                os.mkdir("cache")
-            plot.save(os.path.join("cache", "graph.png"))
+            cache_dir = "cache"
+            if not os.path.exists(cache_dir):
+                os.mkdir(cache_dir)
+            plot.save(os.path.join(cache_dir, "graph.png"))
             self.plot = plot
             self.parent().parent().refresh()
