76f448f3b470a5ae52809c0b263bc9f40d6ce83a
Quân <116066343+quankenz@users.noreply.github.com>
2023-10-07 09:27:23 +0700

    Add files via upload

---
+++ demo/test.py
@@ -0,0 +1,76 @@
+import numpy as np
+from keras_preprocessing import image
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import Label, Canvas, Button
+from tensorflow.keras.models import load_model
+from PIL import Image, ImageTk
+
+# Load the pre-trained model
+new_model = load_model('model.h5')
+classes = ['Chuối', 'Dâu tây', 'Dứa', 'Khế', 'Măng cụt', 'Xoài']
+
+# Function to classify an image
+def classify_image():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        # Load and preprocess the selected image
+        img = Image.open(file_path)
+        img = img.convert('RGB')
+        img = img.resize((64, 64))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+
+        # Predict the class of the image
+        result = new_model.predict(img)
+        result1 = result[0]
+        for y in range(6):
+            if result1[y] == 1.:
+                break
+        prediction = classes[y]
+        
+        # Display the prediction
+        result_label.config(text=f'Đây là quả: {prediction}')
+
+        # Display the selected image in a frame
+        img = Image.open(file_path)
+        img = img.resize((300, 300))
+        img = ImageTk.PhotoImage(img)
+        canvas.create_image(0, 0, anchor=tk.NW, image=img)
+        canvas.image = img
+
+# Function to clear the selected image and prediction
+def clear_image():
+    result_label.config(text="")
+    canvas.delete("all")
+
+# Function to exit the application
+def exit_app():
+    root.destroy()
+
+# Create the main application window
+root = tk.Tk()
+root.title("TRÁI CÂY")
+
+# Create a button to open an image
+open_button = tk.Button(root, text="Chọn ảnh", command=classify_image)
+open_button.pack(pady=10)
+
+# Create a canvas to display the selected image
+canvas = Canvas(root, width=300, height=300)
+canvas.pack()
+
+# Create a label to display the prediction
+result_label = Label(root, text="", font=("Helvetica", 16))
+result_label.pack()
+
+# Create a button to clear the selected image and prediction
+clear_button = tk.Button(root, text="Xóa", command=clear_image)
+clear_button.pack(pady=10)
+
+# Create a button to exit the application
+exit_button = tk.Button(root, text="Thoát", command=exit_app)
+exit_button.pack(pady=10)
+
+# Start the main application loop
+root.mainloop()
