b7fff593a867114f7002f645dd73d08c10c3485d
Nguyễn Đức Quân <116066343+quankenz@users.noreply.github.com>
2023-10-08 17:17:05 +0700

    Update test.py

--- Nhom7_Tensorflow/test.py
+++ Nhom7_Tensorflow/test.py
@@ -1,1 +1,9 @@
 import numpy as np
+from keras_preprocessing import image
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import Label, Canvas, Button, ttk
+from keras.models import load_model
+from PIL import Image,ImageTk
+new_model=load_model('model.keras')
+classes = ['Chuối', 'Dâu tây', 'Dứa', 'Khế', 'Măng cụt', 'Xoài']
