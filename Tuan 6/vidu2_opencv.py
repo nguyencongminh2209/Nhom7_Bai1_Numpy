160c9a49e11954e705d7527ad4773d1a7ba0a8c1
VuThuPhuong <145332969+VuThuPhuong@users.noreply.github.com>
2023-10-17 09:28:29 +0700

    Phuong_up_vidu2_opencv.py

--- Tuan 6/vidu2_opencv.py
+++ Tuan 6/vidu2_opencv.py
@@ -15,13 +15,13 @@ def update_image():
 # Hàm xử lý sự kiện khi nút "Zoom In" được nhấn
 def zoom_in():
     global current_scale
-    current_scale *= 1.2
+    current_scale *= scale_factor
     update_image()
 
 # Hàm xử lý sự kiện khi nút "Zoom Out" được nhấn
 def zoom_out():
     global current_scale
-    current_scale /= 1.2
+    current_scale /= scale_factor
     update_image()
 
 # Hàm xử lý sự kiện khi nút "Chọn ảnh" được nhấn
@@ -33,6 +33,17 @@ def select_image():
         current_scale = 1.0
         update_image()
 
+# Hàm xử lý sự kiện khi nút "Xác nhận" để thay đổi tỷ lệ zoom
+def change_scale():
+    global current_scale
+    scale_text = scale_entry.get()
+    try:
+        scale_value = float(scale_text)
+        current_scale = scale_value
+        update_image()
+    except ValueError:
+        pass
+
 # Tạo cửa sổ
 window = tk.Tk()
 window.title("Zoom In/Out Image")
@@ -40,6 +51,7 @@ window.title("Zoom In/Out Image")
 # Khai báo biến toàn cục
 current_scale = 1.0
 original_img = None
+scale_factor = 1.2
 
 # Tạo nút "Chọn ảnh" và đặt sự kiện khi nút này được nhấn
 select_button = tk.Button(window, text="Chọn ảnh", command=select_image)
@@ -52,6 +64,14 @@ zoom_in_button.pack()
 # Tạo nút "Zoom Out" và đặt sự kiện khi nút này được nhấn
 zoom_out_button = tk.Button(window, text="Zoom Out", command=zoom_out)
 zoom_out_button.pack()
+
+# Tạo hộp văn bản để nhập tỷ lệ zoom
+scale_label = tk.Label(window, text="Nhập tỷ lệ zoom:")
+scale_label.pack()
+scale_entry = tk.Entry(window)
+scale_entry.pack()
+scale_confirm_button = tk.Button(window, text="Xác nhận", command=change_scale)
+scale_confirm_button.pack()
 
 # Tạo label để hiển thị ảnh
 img_label = tk.Label(window)
