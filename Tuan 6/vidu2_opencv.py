aa6c583e1d576d36878e282e8ac0d7b96b4270a4
Minh <145255015+nguyencongminh2209@users.noreply.github.com>
2023-10-17 09:27:13 +0700

    Update vidu2_opencv.py

--- Tuan 6/vidu2_opencv.py
+++ Tuan 6/vidu2_opencv.py
@@ -1,42 +1,60 @@
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
 # Hàm để cập nhật và hiển thị ảnh sau khi zoom
 def update_image():
-    plt.clf()
-    for i in range(count):
-        plt.subplot(2, 2, i + 1)
-        plt.title(Titles[i])
-        plt.imshow(images[i], cmap='gray')
-    plt.show()
+    scaled_img = cv2.resize(original_img, None, fx=current_scale, fy=current_scale, interpolation=cv2.INTER_LINEAR)
+    img = cv2.cvtColor(scaled_img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    img = ImageTk.PhotoImage(img)
+    img_label.config(image=img)
+    img_label.image = img
 
-# Khởi tạo ảnh ban đầu và danh sách các phiên bản sau khi zoom
-original_img = cv2.imread('a.png', cv2.IMREAD_GRAYSCALE)
-half = cv2.resize(original_img, (0, 0), fx=0.1, fy=0.1)
-bigger = cv2.resize(original_img, (1050, 1610))
-stretch_near = cv2.resize(original_img, (780, 540), interpolation=cv2.INTER_LINEAR)
+# Hàm xử lý sự kiện khi nút "Zoom In" được nhấn
+def zoom_in():
+    global current_scale
+    current_scale *= 1.2
+    update_image()
 
-Titles = ["Original", "Half", "Bigger", "Interpolation Nearest"]
-images = [original_img, half, bigger, stretch_near]
-count = 4
+# Hàm xử lý sự kiện khi nút "Zoom Out" được nhấn
+def zoom_out():
+    global current_scale
+    current_scale /= 1.2
+    update_image()
+
+# Hàm xử lý sự kiện khi nút "Chọn ảnh" được nhấn
+def select_image():
+    global original_img, current_scale
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        original_img = cv2.imread(file_path)
+        current_scale = 1.0
+        update_image()
+
+# Tạo cửa sổ
+window = tk.Tk()
+window.title("Zoom In/Out Image")
+
+# Khai báo biến toàn cục
 current_scale = 1.0
+original_img = None
 
-# Hiển thị ban đầu
-update_image()
+# Tạo nút "Chọn ảnh" và đặt sự kiện khi nút này được nhấn
+select_button = tk.Button(window, text="Chọn ảnh", command=select_image)
+select_button.pack()
 
-# Lặp để cho phép người dùng thay đổi ảnh
-while True:
-    choice = input("Nhập '1' để Zoom In, '2' để Zoom Out, hoặc 'q' để thoát: ")
-    if choice == '1':
-        for i in range(count):
-            images[i] = cv2.resize(images[i], (0, 0), fx=1.2, fy=1.2)
-        current_scale *= 1.2
-        update_image()
-    elif choice == '2':
-        for i in range(count):
-            images[i] = cv2.resize(images[i], (0, 0), fx=1/1.2, fy=1/1.2)
-        current_scale /= 1.2
-        update_image()
-    elif choice == 'q':
-        break
+# Tạo nút "Zoom In" và đặt sự kiện khi nút này được nhấn
+zoom_in_button = tk.Button(window, text="Zoom In", command=zoom_in)
+zoom_in_button.pack()
+
+# Tạo nút "Zoom Out" và đặt sự kiện khi nút này được nhấn
+zoom_out_button = tk.Button(window, text="Zoom Out", command=zoom_out)
+zoom_out_button.pack()
+
+# Tạo label để hiển thị ảnh
+img_label = tk.Label(window)
+img_label.pack()
+
+window.mainloop()
