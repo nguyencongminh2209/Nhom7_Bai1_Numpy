5657c63a966466beec4f1ecb2f89ebfe39aecf1b
Minh <145255015+nguyencongminh2209@users.noreply.github.com>
2023-10-24 09:51:01 +0700

    Minh_updatecode.py

--- Tuan 7/code.py
+++ Tuan 7/code.py
@@ -26,3 +26,13 @@ def select_region(event, x, y, flags, param):
         cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
 
         selected_region = img[start_y:end_y, start_x:end_x]
+def open_image():
+    global img, selected_region
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp *.gif *.tif")])
+    if file_path:
+        img = cv2.imread(file_path)
+        if img is not None:
+            cv2.namedWindow('Portrait')
+            cv2.setMouseCallback('Portrait', select_region)
+            selected_region = None
+            cv2.imshow('Portrait', img)
